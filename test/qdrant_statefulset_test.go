99de33f6159ac7d0ce9f22cbe899eff1f16e7cb4
Robert Marsal <507871+robertmarsal@users.noreply.github.com>
2025-10-22 14:56:46 +0100

    feat: add support for `volumeAttributesClass` for pvcs (#396)
    closes #365

--- test/qdrant_statefulset_test.go
+++ test/qdrant_statefulset_test.go
@@ -1,15 +1,16 @@
 package test
 
 import (
+	"path/filepath"
+	"strings"
+	"testing"
+
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
-	"path/filepath"
-	"strings"
-	"testing"
 )
 
 func TestContainerSecurityContextUserAndGroupDefault(t *testing.T) {
@@ -68,4 +69,35 @@ func TestContainerSecurityContextUserAndGroupLargeValues(t *testing.T) {
 	require.Equal(t, int64(1000640000), *statefulSet.Spec.Template.Spec.Containers[0].SecurityContext.RunAsUser)
 	require.Equal(t, int64(1000740000), *statefulSet.Spec.Template.Spec.SecurityContext.FSGroup)
 	require.Equal(t, int64(1000840000), *statefulSet.Spec.Template.Spec.Containers[0].SecurityContext.RunAsGroup)
+}
+
+func TestCanAttachVolumeAttributesClassToPersistence(t *testing.T) {
+	t.Parallel()
+
+	helmChartPath, err := filepath.Abs("../charts/qdrant")
+	releaseName := "qdrant"
+	require.NoError(t, err)
+
+	namespaceName := "qdrant-" + strings.ToLower(random.UniqueId())
+	logger.Log(t, "Namespace: %s\n", namespaceName)
+
+	options := &helm.Options{
+		SetJsonValues: map[string]string{
+			"persistence": `{
+				"volumeAttributesClassName": "my-class"
+			}`,
+			"snapshotPersistence": `{
+				"enabled": true,
+				"volumeAttributesClassName": "my-class"
+			}`,
+		},
+		KubectlOptions: k8s.NewKubectlOptions("", "", namespaceName),
+	}
+
+	statefulsetOutput := helm.RenderTemplate(t, options, helmChartPath, releaseName, []string{"templates/statefulset.yaml"})
+
+	var statefulSet appsv1.StatefulSet
+	helm.UnmarshalK8SYaml(t, statefulsetOutput, &statefulSet)
+	require.Equal(t, "my-class", *statefulSet.Spec.VolumeClaimTemplates[0].Spec.VolumeAttributesClassName)
+	require.Equal(t, "my-class", *statefulSet.Spec.VolumeClaimTemplates[1].Spec.VolumeAttributesClassName)
 }
